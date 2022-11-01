4fad51f7c0b24511d5d7e3c8eb8b32adc0ad4026
Muhammad Hasham <hasham25525@gmail.com>
2022-11-01 15:30:10 +0500

    Chapter 1 - 8

---
+++ JAVAScript first assignment/app.js
@@ -0,0 +1,83 @@
+
+/*********************************chap 1 & 2 ***************************** */
+/***************************************************************************/
+var thanx = "Thanks for your input!";
+alert(thanx);
+
+var nationality;
+nationality = "U.S.";
+console.log(nationality);
+
+/***********************************chap 3******************************** */
+/***************************************************************************/
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+console.log("newNum = " + newNum);
+// increament
+var originalNum = 90;
+originalNum = originalNum + 10;
+console.log("originalNum = " + originalNum);
+
+
+/***********************************chap 4******************************** */
+/***************************************************************************/
+
+/****illegal variable names****
+var 1num; 
+var -num;
+var .num;
+var var;
+************/
+
+/****legal variable names****
+var num;
+var Num; 
+var num1;
+var num_2;
+var numOne
+************/
+
+
+/***********************************chap 5******************************** */
+/***************************************************************************/
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+console.log("popularNumber = " + popularNumber);
+
+var whatsLeftOver = 9 % 3;
+console.log(" 9%3 = " + whatsLeftOver);
+
+
+
+/***********************************chap 6******************************** */
+/***************************************************************************/
+var Num = 10;
+Num++;
+console.log("Incremented " + Num);
+console.log("PreDecrement " + --Num);
+
+
+/***********************************chap 7******************************** */
+/***************************************************************************/
+var totalCost = 1 + 3 * 4;
+console.log("1+3*4 = " + totalCost);
+var totalCost2 = (1 + 3) * 4;
+console.log("(1+3)*4 = " + totalCost2);
+
+
+
+/***********************************chap 8******************************** */
+/***************************************************************************/
+var message = "Thanks, ";
+var userName = "Susan";
+var banger = "!";
+var customMess = message + userName + banger;
+alert(customMess);
+
+// concatenation
+var a = "21"
+var b = "34"
+console.log('concatenation =' + a + b);
+
